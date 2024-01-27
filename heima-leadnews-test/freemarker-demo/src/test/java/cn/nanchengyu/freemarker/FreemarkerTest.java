cc907dc6667ab7cc5da4808d3d61f3d151482d1a
nanchengcyu <1146137024@qq.com>
2024-01-28 00:45:18 +0800

    freemarker study

---
+++ heima-leadnews-test/freemarker-demo/src/test/java/cn/nanchengyu/freemarker/FreemarkerTest.java
@@ -0,0 +1,13 @@
+package cn.nanchengyu.freemarker;
+
+/**
+ * ClassName: test
+ * Package: cn.nanchengyu.freemarker
+ * Description:
+ *
+ * @Author 南城余
+ * @Create 2024/1/27 23:36
+ * @Version 1.0
+ */
+public class FreemarkerTest{
+}
