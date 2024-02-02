7e0ae6bda9ba1df66fe636deb46abac14abf9de9
nanchengcyu <1146137024@qq.com>
2024-02-02 16:54:09 +0800

    用户搜索记录 mongodb

--- heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/service/impl/ApUserSearchServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/service/impl/ApUserSearchServiceImpl.java
@@ -25,42 +25,44 @@ public class ApUserSearchServiceImpl implements ApUserSearchService {
 
     @Autowired
     private MongoTemplate mongoTemplate;
+
     /**
      * 保存用户搜索历史记录
+     *
      * @param keyword
      * @param userId
      */
     @Override
     @Async
     public void insert(String keyword, Integer userId) {
-        //1.查询当前用户的搜索关键词
+        //1. 查询当前用户得搜索关键词
+        //创建查询对象以及搜索条件
         Query query = Query.query(Criteria.where("userId").is(userId).and("keyword").is(keyword));
         ApUserSearch apUserSearch = mongoTemplate.findOne(query, ApUserSearch.class);
 
-        //2.存在 更新创建时间
-        if(apUserSearch != null){
+
+        //2. 存在 更新创建时间
+
+        if (apUserSearch != null) {
             apUserSearch.setCreatedTime(new Date());
             mongoTemplate.save(apUserSearch);
             return;
         }
-
-        //3.不存在，判断当前历史记录总数量是否超过10
+        //3.不存在 判断当前的历史记录总数量是否超过10
         apUserSearch = new ApUserSearch();
         apUserSearch.setUserId(userId);
         apUserSearch.setKeyword(keyword);
         apUserSearch.setCreatedTime(new Date());
-
         Query query1 = Query.query(Criteria.where("userId").is(userId));
         query1.with(Sort.by(Sort.Direction.DESC,"createdTime"));
         List<ApUserSearch> apUserSearchList = mongoTemplate.find(query1, ApUserSearch.class);
-
-        if(apUserSearchList == null || apUserSearchList.size() < 10){
+        if (apUserSearchList == null || apUserSearchList.size()<10){
             mongoTemplate.save(apUserSearch);
         }else {
             ApUserSearch lastUserSearch = apUserSearchList.get(apUserSearchList.size() - 1);
+
             mongoTemplate.findAndReplace(Query.query(Criteria.where("id").is(lastUserSearch.getId())),apUserSearch);
         }
-
 
     }
 
@@ -79,7 +81,7 @@ public class ApUserSearchServiceImpl implements ApUserSearchService {
 
         //根据用户查询数据，按照时间倒序
         List<ApUserSearch> apUserSearches = mongoTemplate.find(Query.query(Criteria.where("userId")
-                .is(user.getId()))
+                        .is(user.getId()))
                 .with(Sort.by(Sort.Direction.DESC, "createdTime")), ApUserSearch.class);
         return ResponseResult.okResult(apUserSearches);
     }
@@ -93,18 +95,18 @@ public class ApUserSearchServiceImpl implements ApUserSearchService {
     @Override
     public ResponseResult delUserSearch(HistorySearchDto dto) {
         //1.检查参数
-        if(dto.getId() == null){
+        if (dto.getId() == null) {
             return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
         }
 
         //2.判断是否登录
         ApUser user = AppThreadLocalUtil.getUser();
-        if(user == null){
+        if (user == null) {
             return ResponseResult.errorResult(AppHttpCodeEnum.NEED_LOGIN);
         }
 
         //3.删除
-        mongoTemplate.remove(Query.query(Criteria.where("userId").is(user.getId()).and("id").is(dto.getId())),ApUserSearch.class);
+        mongoTemplate.remove(Query.query(Criteria.where("userId").is(user.getId()).and("id").is(dto.getId())), ApUserSearch.class);
         return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
     }
 }
